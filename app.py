6310c707d034b9a03cc9384c25c8cc002751e1ac
root <root@ip-10-0-2-147.us-west-2.compute.internal>
2026-07-15 11:02:07 +0000

    connection through host

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def home():
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM Asamy LIMIT 1;")
+    row = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    if row:
+        return f"ID: {row[0]}, Name: {row[1]}"
+    else:
+        return "No rows found."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
